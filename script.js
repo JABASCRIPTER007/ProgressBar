7a07cb09c70814e961067679c653ee99757c7737
ANDRII <105977866+JABASCRIPTER007@users.noreply.github.com>
2023-06-22 12:16:46 +0300

    added comments in < script.js >

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Get references to HTML elements
 let selectGenerator = document.getElementById("selectGenerator");
 let generatorCount = document.getElementById('generatorCount');
 let generatorPower = document.getElementById('generatorPower');
@@ -9,18 +10,22 @@ let timeOfWork = document.getElementById('timeOfWork');
 let file = document.getElementById('file');
 let progressNumber = document.getElementById('progress-number');
 
+// Function to perform calculations and update values
 function myFunction() {
+    // Get values from input elements
     let generatorPowerValue = selectGenerator.value;
     let count = generatorCount.value;
 
     let consumerPower = consumer.value;
-    let consumerCountValue = consumerCount.value
+    let consumerCountValue = consumerCount.value;
     let consumerTimeOfWork = timeOfWork.value;
 
+    // Perform calculations if generator power and count are provided
     if (generatorPowerValue && count) {
         let generalPower = generatorPowerValue * count;
         generatorPower.innerHTML = generalPower;
 
+        // Perform further calculations if consumer power, count, and time of work are provided
         if (consumerPower && consumerCountValue && consumerTimeOfWork) {
             let generalConsumerValue = (consumerPower * consumerCountValue * consumerTimeOfWork) / 60;
 
@@ -29,4 +34,3 @@ function myFunction() {
         }
     }
 }
-
